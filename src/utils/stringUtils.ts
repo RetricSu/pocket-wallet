5550ef70eb3e62c07fb98e24a71652d0b931c6f3
RetricSu <inneverland2013@gmail.com>
2025-04-22 10:35:06 +0800

    add linter

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,19 +1,11 @@
-export function truncateString(
-  str: string,
-  frontChars: number,
-  endChars: number
-): string {
+export function truncateString(str: string, frontChars: number, endChars: number): string {
   if (str.length <= frontChars + endChars) {
     return str;
   }
   return `${str.slice(0, frontChars)}...${str.slice(-endChars)}`;
 }
 
-export function truncateAddress(
-  address: string,
-  frontChars: number = 6,
-  endChars: number = 4
-): string {
+export function truncateAddress(address: string, frontChars: number = 6, endChars: number = 4): string {
   return truncateString(address, frontChars, endChars);
 }
 
