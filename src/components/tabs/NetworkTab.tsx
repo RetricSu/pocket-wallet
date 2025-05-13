d9e84c0e55684803caf4e0ddb6f3943b9c643fb1
RetricSu <inneverland2013@gmail.com>
2025-05-13 15:32:09 +0800

    chore wording

--- src/components/tabs/NetworkTab.tsx
+++ src/components/tabs/NetworkTab.tsx
@@ -34,10 +34,9 @@ export const NetworkTab: React.FC<NetworkTabProps> = ({}) => {
         </div>
         <div className="text-text-secondary text-sm">
           <p>
-            CKB is a public blockchain based on POW and UTXO-like data structure. Pocket Wallet use CKB Light Client to
-            sync the blockchain right in your browser without relying on any centralized RPC service. By integrating
-            Nostr with Nip46 and more, Pocket Wallet provides a fully verifiable wallet experience in the methodology of
-            client-side-validation and POW verifications.
+            Pocket Wallet use CKB Light Client to sync the blockchain right in your browser without relying on any
+            centralized RPC service. By integrating Nostr with Nip46 and more, Pocket Wallet provides a fully verifiable
+            wallet experience in the methodology of client-side-validation and POW verifications.
           </p>
         </div>
       </div>
